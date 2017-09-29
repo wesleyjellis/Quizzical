f9e3724306a1c725484fa289c7711f09e55d83b8
Wesley Ellis <wesley.ellis@shopify.com>
2017-09-29 14:40:55 -0400

    WIP

--- app/src/main/java/com/shopify/quizzical/MainActivity.java
+++ app/src/main/java/com/shopify/quizzical/MainActivity.java
@@ -3,11 +3,14 @@ package com.shopify.quizzical;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import org.libsodium.jni.Sodium;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Sodium.crypto_aead_chacha20poly1305_abytes();
     }
 }
