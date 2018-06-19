516f532df53eb49af82d88e74530a94560893a9c
Wesley Ellis <wesley.ellis@shopify.com>
2018-06-19 09:36:21 -0400

    part 3: manipulating views

--- app/src/main/java/com/example/wes/quizzical/MainActivity.java
+++ app/src/main/java/com/example/wes/quizzical/MainActivity.java
@@ -2,12 +2,41 @@ package com.example.wes.quizzical;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button trueButton;
+    private Button falseButton;
+    private TextView answerTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        trueButton = findViewById(R.id.true_button);
+        falseButton = findViewById(R.id.false_button);
+        answerTextView = findViewById(R.id.answer_text);
+
+        trueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("HI", "true button clicked");
+                answerTextView.setText("Nope, WRONG 👎");
+            }
+        });
+
+        falseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("HI", "false button clicked");
+                answerTextView.setText("Correct!");
+
+            }
+        });
     }
 }
